7824accc943e823d6d85acb9f34843be53935343
Maria Muna <umuna201429@bscse.uiu.ac.bd>
2022-11-16 13:54:30 +0600

    SignIn activity Updated

--- Client/src/main/java/com/application/client/SignIn.java
+++ Client/src/main/java/com/application/client/SignIn.java
@@ -7,11 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class SignIn extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(SignIn.class.getResource("sign-in-activity.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400
+        );
         stage.setTitle("Hello!");
         stage.setScene(scene);
 
