b5deea4e7bfc32e15aaa8c6cd03e8ba86963450c
Kishorè Shanto <tinykishore@gmail.com>
2022-12-08 02:23:56 +0600

    Documented `Controller.java`

--- Client/src/main/java/com/application/client/Controller.java
+++ Client/src/main/java/com/application/client/Controller.java
@@ -11,17 +11,46 @@ import javafx.stage.Stage;
  * @author Kishorè Shanto
  */
 public class Controller extends Application {
+
+    /**
+     * DO NOT EDIT THIS METHOD
+     *
+     * This method is called when the application should stop, and provides a
+     * convenient place to prepare for application exit and destroy resources.
+     *
+     * <p>
+     * The implementation of this method provided by the Application class does nothing.
+     * </p>
+     *
+     * @param stage the primary stage for this application, onto which
+     * the application scene can be set.
+     * Applications may create other stages, if needed, but they will not be
+     * primary stages.
+     */
     @Override
     public void start(Stage stage) {
-        // DO NOT REMOVE THIS METHOD
-        // DO NOT ADD ANY CODE TO THIS METHOD
+
     }
 
+    /**
+     * The application initialization method. This method is called immediately
+     * after the Application class is loaded and constructed. An application may
+     * override this method to perform initialization prior to the actual starting
+     * of the application.
+     * @throws Exception
+     */
     @Override
     public void init() throws Exception {
         super.init();
     }
 
+    /**
+     * The application cleanup method. This method is called after the application
+     * has stopped, and its primary stage has been removed from the screen. An
+     * application may override this method to perform any necessary cleanup
+     * operations.
+     * @throws Exception
+     */
     @Override
     public void stop() throws Exception {
         super.stop();
