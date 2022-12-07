b9ca83308570abd21f0cb17e1587ec53513c5802
Maria Muna <umuna201429@bscse.uiu.ac.bd>
2022-12-07 21:14:21 +0600

    network request

---
+++ Client/src/main/java/com/application/utils/NetworkRequest.java
@@ -0,0 +1,13 @@
+package com.application.utils;
+
+public class NetworkRequest {
+
+    public static final int SIGN_IN_REQUEST = 20;
+
+    public static final int USER_FOUND_FROM_DATABASE = 30;
+
+    public static final int USER_NOT_FOUND_FROM_DATABASE = 40;
+
+    public static final int SIGN_UP_REQUEST = 10;
+
+}
