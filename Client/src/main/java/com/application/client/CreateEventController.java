f8bfa26d732170624a023b350b0427f2fbda5893
Maria Muna <umuna201429@bscse.uiu.ac.bd>
2022-12-08 23:16:19 +0600

    create gui and button

---
+++ Client/src/main/java/com/application/client/CreateEventController.java
@@ -0,0 +1,33 @@
+package com.application.client;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class CreateEventController {
+    public TextField create_event_name;
+    public TextField create_event_category;
+    public TextArea create_event_description;
+    public DatePicker create_event_date;
+    public TextField create_event_start_time;
+    public TextField create_event_end_time;
+
+    public void onCreateButtonListener(ActionEvent actionEvent) {
+        String event_name = create_event_name.getText();
+        String event_category = create_event_category.getText();
+        String event_description = create_event_description.getText();
+        String event_date = create_event_date.getValue().toString();
+        String event_start_time = create_event_start_time.getText();
+        String event_end_time = create_event_end_time.getText();
+
+        Event event = new Event("emailnaiii", event_name, event_description,event_date,
+                Integer.parseInt(event_start_time), Integer.parseInt(event_end_time), event_category);
+
+        System.out.println(event);
+    }
+
+    public void onCancelButtonListener(ActionEvent actionEvent) {
+
+    }
+}
--- Client/src/main/java/com/application/client/HomeActivityController.java
+++ Client/src/main/java/com/application/client/HomeActivityController.java
@@ -2,11 +2,27 @@
 
 package com.application.client;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * This Controller handles the Home Activity.
  * @author Kishorè Shanto
  */
-public class HomeActivityController extends Controller {
+public class HomeActivityController extends Controller{
 
+    public void onCreateEventButton(ActionEvent actionEvent) throws Exception {
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("create-event.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load());
+
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
 
