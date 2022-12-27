eca64a18c89702c99b8233fa0962b48e663f5de0
Kishorè Shanto <tinykishore@gmail.com>
2022-12-27 21:43:00 +0600

    update demo

--- Client/src/main/java/com/application/serialShared/Event.java
+++ Client/src/main/java/com/application/serialShared/Event.java
@@ -1,5 +1,8 @@
 package com.application.serialShared;
 
+import com.application.utility.Date;
+import com.application.utility.Time;
+
 import java.io.Serializable;
 
 /**
@@ -15,6 +18,6 @@ import java.io.Serializable;
  * @param event_end_time End time of the event
  */
 public record Event(String user_email, String event_name, String event_description, String event_category,
-                    String event_date, int event_start_time, int event_end_time) implements Serializable {
+                    Date event_date, Time event_start_time, Time event_end_time) implements Serializable {
 
 }
